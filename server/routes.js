e03d8daec7511e26fd680575552dc4ecf3d23c1e
marekkocurik <marek.kocurik02@gmail.com>
2020-04-27 23:22:28 +0200

    first try to call multiple requests

--- server/routes.js
+++ server/routes.js
@@ -6,62 +6,76 @@ function parseXML(xml, method) {
   return json['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:' + method + 'Response'];
 }
 
+async function getImages(id) {
+
+
+}
+
 async function routes(fastify) {
-  fastify.get('/calendar', async (req, res) => {
-    let day = req.query.day;
-    let month = req.query.month;
-    let options = {
-      'method': 'POST',
-      'url': 'http://pis.predmety.fiit.stuba.sk/pis/ws/Calendar',
-      'headers': {
-        'Content-Type': ['text/xml', 'application/xml']
-      },
-      'body': `<?xml version="1.0" encoding="utf-8"?>
-               <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/calendar/types">
-                 <soapenv:Header/>
-                 <soapenv:Body>
-                   <typ:getSunrise>
-                     <month>${month}</month>
-                     <day>${day}</day>
-                   </typ:getSunrise>
-                 </soapenv:Body>
-               </soapenv:Envelope>`
-
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      res.send(parseXML(response, 'getSunrise'));
-    });
-
-  })
-
-  fastify.get('/cities', async (req, res) => {
-    let name = req.query.name;
-    let options = {
-      'method': 'POST',
-      'url': 'http://pis.predmety.fiit.stuba.sk/pis/ws/GeoServices/CitiesSK',
-      'headers': {
-        'Content-Type': ['text/xml', 'application/xml']
-      },
-      'body': `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/geoservices/citiessk/types">
-                 <soapenv:Header/>
-                 <soapenv:Body>
-                   <typ:searchByName>
-                     <name>${name}</name>
-                   </typ:searchByName>
-                 </soapenv:Body>
-               </soapenv:Envelope>`
-
-    };
-    request(options, function (error, response) {
-      if (error) throw new Error(error);
-      res.send(parseXML(response, 'searchByName'));
-    });
-
-  })
-
 //odtialto zacinaju bengere
 
+fastify.get('/games', async (req, res) => {
+  let gamesResponse;
+  let hra_id;
+  let gamesRequest = {
+    'method': 'POST',
+    'url': 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106hra',
+    'headers': {
+      'Content-Type': ['text/xml', 'application/xml']
+    },
+    'body': `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106hra/types">
+              <soapenv:Header/>
+              <soapenv:Body>
+                <typ:getAll/>
+              </soapenv:Body>
+            </soapenv:Envelope>`
+  };
+
+
+  request(gamesRequest, function (error, response) {
+    if (error) throw new Error(error);
+    gamesResponse = parseXML(response, 'getAll');
+    gamesResponse = gamesResponse.hras.hra;
+
+    for(let i = 0; i < gamesResponse.length; i++) {
+      hra_id = gamesResponse[i].id;
+      let imagesRequest = {
+        'method': 'POST',
+        'url': 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106obrazok',
+        'headers': {
+          'Content-Type': ['text/xml', 'application/xml']
+        },
+        'body': `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106obrazok/types">
+                   <soapenv:Header/>
+                   <soapenv:Body>
+                      <typ:getByAttributeValue>
+                         <attribute_name>hra_id</attribute_name>
+                         <attribute_value>${hra_id}</attribute_value>
+                         <ids>
+                            <id></id>
+                         </ids>
+                      </typ:getByAttributeValue>
+                   </soapenv:Body>
+                </soapenv:Envelope>`
+      };
+
+      gamesResponse[i].obrazky = [];
+      let images;
+      request(imagesRequest, function (error, response) {
+        if (error) throw new Error(error);
+        images = parseXML(response, 'getByAttributeValue');
+        images = images.obrazoks.obrazok;
+        images.forEach(img => {
+          gamesResponse[i].obrazky.push(img.img);
+        })
+
+        if(i === gamesResponse.length - 1)
+          res.send(gamesResponse);
+      });
+    }
+  });
+
+})
 
 }
 
