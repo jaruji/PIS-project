d76818a058cd1da35e1619ff59d37a0e3f225e5d
jaruji <jurajbedej1@yahoo.com>
2020-04-28 04:13:48 +0200

    Made /games lighter + new endpoint based on game id

--- server/routes.js
+++ server/routes.js
@@ -6,45 +6,42 @@ function parseXML(xml, method) {
   return json['SOAP-ENV:Envelope']['SOAP-ENV:Body']['ns1:' + method + 'Response'];
 }
 
-function doRequest(req) {
-
-  return new Promise(function(resolve, reject) {
-    request(req.body, function (error, response, body) {
+function doRequest(req){
+  return new Promise(function(resolve, reject){
+    request(req.body, function (error, response, body){
       if (error) reject(error);
       resolve(parseXML(response, req.method));
     });
   });
 }
 
-async function routes(fastify) {
-//odtialto zacinaju bengere
-
-fastify.get('/games', async (req, res) => {
-  let gamesResponse;
-  let hra_id;
-
-  let gamesRequest = {
-    method: 'getAll',
-    body: {
-      method: 'POST',
-      url: 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106hra',
-      headers: {
-        'Content-Type': ['text/xml', 'application/xml']
-      },
-      body: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106hra/types">
-                <soapenv:Header/>
-                <soapenv:Body>
-                  <typ:getAll/>
-                </soapenv:Body>
-              </soapenv:Envelope>`
-    }
-  };
-
-  gamesResponse = await doRequest(gamesRequest);
-  gamesResponse = gamesResponse.hras.hra;
-  for(let i in gamesResponse) {
-    hra_id = gamesResponse[i].id;
-    gamesResponse[i].obrazky = [];
+async function routes(fastify){
+  fastify.get('/game', async (req, res) => {
+    let hra_id = req.query.id;
+    let gamesResponse = await doRequest({
+      method: 'getByAttributeValue',
+      body: {
+        method: 'POST',
+        url: 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106hra',
+        headers: {
+          'Content-Type': ['text/xml', 'application/xml']
+        },
+        body: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106hra/types">
+                     <soapenv:Header/>
+                     <soapenv:Body>
+                        <typ:getByAttributeValue>
+                           <attribute_name>id</attribute_name>
+                           <attribute_value>${hra_id}</attribute_value>
+                           <ids>
+                              <id></id>
+                           </ids>
+                        </typ:getByAttributeValue>
+                     </soapenv:Body>
+                  </soapenv:Envelope>`
+      }
+    });
+    gamesResponse = gamesResponse.hras.hra;
+    gamesResponse.obrazky = [];
     // console.log(imagesRequest.hra_id);
     let images = await doRequest({
       method: 'getByAttributeValue',
@@ -69,12 +66,11 @@ fastify.get('/games', async (req, res) => {
       }
     });
     images = images.obrazoks.obrazok;
-
     if(images.length === undefined)
-      gamesResponse[i].obrazky.push(images.img)
-    else if(images !== null) {
+      gamesResponse.obrazky.push(images.img)
+    else if(images !== null){
       images.forEach(img => {
-        gamesResponse[i].obrazky.push(img.img);
+        gamesResponse.obrazky.push(img.img);
       })
     }
 
@@ -112,8 +108,8 @@ fastify.get('/games', async (req, res) => {
       })
     }
 
-    gamesResponse[i].kategorie = [];
-    for(let j in category_ids) {
+    gamesResponse.kategorie = [];
+    for(let i in category_ids){
       let category = await doRequest({
         method: 'getByAttributeValue',
         body: {
@@ -127,7 +123,7 @@ fastify.get('/games', async (req, res) => {
                    <soapenv:Body>
                       <typ:getByAttributeValue>
                          <attribute_name>id</attribute_name>
-                         <attribute_value>${category_ids[j]}</attribute_value>
+                         <attribute_value>${category_ids[i]}</attribute_value>
                          <ids>
                             <id></id>
                          </ids>
@@ -136,17 +132,67 @@ fastify.get('/games', async (req, res) => {
                   </soapenv:Envelope>`
         }
       });
-
       category = category.kategorias.kategoria;
-      gamesResponse[i].kategorie.push(category.typ);
+      gamesResponse.kategorie.push(category.typ);
     }
-  }
+    res.send(gamesResponse);
+  })
 
-
-
-  res.send(gamesResponse);
-})
-
+  fastify.get('/games', async(req, res) => {
+    let gamesResponse = await doRequest({
+      method: 'getAll',
+      body: {
+        method: 'POST',
+        url: 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106hra',
+        headers: {
+          'Content-Type': ['text/xml', 'application/xml']
+        },
+        body: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106hra/types">
+                  <soapenv:Header/>
+                  <soapenv:Body>
+                    <typ:getAll/>
+                  </soapenv:Body>
+                </soapenv:Envelope>`
+      }
+    });
+    gamesResponse = gamesResponse.hras.hra;
+    let hra_id = null
+    for(let i in gamesResponse){
+      hra_id = gamesResponse[i].id
+      gamesResponse[i].obrazky = []
+      let images = await doRequest({
+        method: 'getByAttributeValue',
+        body: {
+          method: 'POST',
+          url: 'http://pis.predmety.fiit.stuba.sk/pis/ws/Students/Team106obrazok',
+          headers: {
+            'Content-Type': ['text/xml', 'application/xml']
+          },
+          body: `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://pis.predmety.fiit.stuba.sk/pis/students/team106obrazok/types">
+                     <soapenv:Header/>
+                     <soapenv:Body>
+                        <typ:getByAttributeValue>
+                           <attribute_name>hra_id</attribute_name>
+                           <attribute_value>${hra_id}</attribute_value>
+                           <ids>
+                              <id></id>
+                           </ids>
+                        </typ:getByAttributeValue>
+                     </soapenv:Body>
+                  </soapenv:Envelope>`
+        }
+      });
+      images = images.obrazoks.obrazok;
+      if(images.length === undefined)
+        gamesResponse[i].obrazky.push(images.img)
+      else if(images !== null){
+        images.forEach(img => {
+          gamesResponse[i].obrazky.push(img.img);
+        })
+      }
+    }
+    res.send(gamesResponse)
+  })
 }
 
 module.exports = routes
